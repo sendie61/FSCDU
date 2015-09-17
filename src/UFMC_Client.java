7213205d1f10f050a7c6623abf92c1e80c9014da
Frits Senders <frits@thuisnet.info>
2015-09-17 16:40:35 +0200

    quit implemented

--- src/UFMC_Client.java
+++ src/UFMC_Client.java
@@ -1,6 +1,13 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class UFMC_Client {
@@ -37,6 +44,24 @@ public class UFMC_Client {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmQuit = new JMenuItem("Quit");
+		mntmQuit.addActionListener(new ActionListener() {
+	        boolean alreadyDisposed = false;
+			public void actionPerformed(ActionEvent arg0) {
+	            if (frame.isDisplayable()) {
+	                alreadyDisposed = true;
+	                frame.dispose();
+	            }
+			}
+		});
+		mnFile.add(mntmQuit);
 	}
 
 }
