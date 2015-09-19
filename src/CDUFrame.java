f52fc29271534ec3f8a7e11e292e890fd3c0ee6f
Frits Senders <frits@thuisnet.info>
2015-09-19 14:43:10 +0200

    Scaling..,.

--- src/CDUFrame.java
+++ src/CDUFrame.java
@@ -4,9 +4,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import java.awt.Rectangle;
 import java.awt.Window.Type;
 import java.awt.Color;
 import java.awt.Dialog.ModalExclusionType;
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import java.awt.Font;
 
 //
 // CDUFrame represents the actual content of the FMC module
@@ -14,21 +22,59 @@ import java.awt.Dialog.ModalExclusionType;
 public class CDUFrame extends JFrame {
 
 	private JPanel contentPane;
-
+	private JLabel[] line; 
+	private boolean largeFont[]={true,false,true,false,true,false,true,false,true,false,true,false,true,true};
 	/**
 	 * Create the frame.
 	 */
 	public CDUFrame() {
 		initialize();
 	}
-	
+
 	private void initialize() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 400, 300);
-		setUndecorated(true);  // No title bar nor border
+		setUndecorated(true); // No title bar nor border
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.BLACK);
-		setContentPane(contentPane);		
+		contentPane.setForeground(new Color(50, 205, 50));
+		contentPane.setBackground(new Color(0, 0, 0));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JLabel lblEmptyLabel = new JLabel("");
+		contentPane.add(lblEmptyLabel);
+		
+		int i=0;		
+		line = new JLabel[20];
+		while (i < 14){
+			line[i] = new JLabel("0123456789012345678901234");
+			line[i].setHorizontalAlignment(SwingConstants.CENTER);
+			if (largeFont[i]){
+				line[i].setFont(new Font("AEROWINX FMC LARGE", Font.PLAIN, 400/16));
+			}
+			else{
+				line[i].setFont(new Font("AEROWINX FMC SMALL", Font.PLAIN, 400/16));
+			}
+			line[i].setForeground(new Color(0, 204, 0));
+			contentPane.add(line[i]);
+			i++;
+		}
+		JLabel lblEmptyLabel2 = new JLabel("");
+		contentPane.add(lblEmptyLabel2);
 	}
 
+	public void redraw( Rectangle rect) {
+		int i=0;
+		while (i < 14){
+			if (largeFont[i]){
+				line[i].setFont(new Font("AEROWINX FMC LARGE", Font.PLAIN, rect.width/16));
+			}
+			else{
+				line[i].setFont(new Font("AEROWINX FMC SMALL", Font.PLAIN, rect.width/16));
+			}
+			i++;
+		}
+		this.setBounds(rect);
+	}
 }
--- src/UFMC_Client.java
+++ src/UFMC_Client.java
@@ -1,4 +1,5 @@
 import java.awt.EventQueue;
+import java.awt.Rectangle;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
@@ -31,7 +32,6 @@ import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-
 //import java.awt.*;
 //import javax.swing.*;
 
@@ -43,6 +43,7 @@ public class UFMC_Client {
 	private JTextField nLeft;
 	private JTextField nHeight;
 	private JTextField nWidth;
+
 	/**
 	 * Launch the application.
 	 */
@@ -75,7 +76,7 @@ public class UFMC_Client {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JButton btnApply = new JButton("Apply");
 		btnApply.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -84,80 +85,83 @@ public class UFMC_Client {
 		});
 		btnApply.setBounds(182, 237, 89, 23);
 		frame.getContentPane().add(btnApply);
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(0, 0, 444, 226);
 		frame.getContentPane().add(tabbedPane);
-		
+
 		JPanel tabLooks = new JPanel();
 		tabbedPane.addTab("Looks", null, tabLooks, null);
-		
+
 		JPanel tabPos = new JPanel();
 		tabbedPane.addTab("Position", null, tabPos, null);
 		tabPos.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("CDU window");
 		lblNewLabel.setForeground(Color.GRAY);
 		lblNewLabel.setBounds(10, 11, 70, 14);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 		tabPos.add(lblNewLabel);
-		
+
 		JLabel lblTop = new JLabel("Top:");
 		lblTop.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblTop.setBounds(90, 11, 46, 14);
 		tabPos.add(lblTop);
-		
+
 		JLabel lblLeft = new JLabel("Left:");
 		lblLeft.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblLeft.setBounds(90, 36, 46, 14);
 		tabPos.add(lblLeft);
-		
+
 		JLabel lblHeight = new JLabel("Height:");
 		lblHeight.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblHeight.setBounds(232, 11, 46, 14);
 		tabPos.add(lblHeight);
-		
+
 		JLabel lblWidth = new JLabel("Width:");
 		lblWidth.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblWidth.setBounds(232, 36, 46, 14);
 		tabPos.add(lblWidth);
-		
+
 		nTop = new JTextField();
 		nTop.setText("25");
 		nTop.setBounds(146, 8, 46, 20);
 		tabPos.add(nTop);
 		nTop.setColumns(10);
-		
+
 		nLeft = new JTextField();
 		nLeft.setText("25");
 		nLeft.setColumns(10);
 		nLeft.setBounds(146, 33, 46, 20);
 		tabPos.add(nLeft);
-		
+
 		nHeight = new JTextField();
 		nHeight.setText("300");
 		nHeight.setColumns(10);
 		nHeight.setBounds(288, 8, 46, 20);
 		tabPos.add(nHeight);
-		
+
 		nWidth = new JTextField();
 		nWidth.setText("400");
 		nWidth.setColumns(10);
 		nWidth.setBounds(288, 33, 46, 20);
 		tabPos.add(nWidth);
-		
+
 		JPanel tabNetwork = new JPanel();
 		tabbedPane.addTab("Network", null, tabNetwork, null);
-		
+
 		CDUWindow = new CDUFrame();
 		CDUWindow.setVisible(true);
 	}
-	
+
 	private void drawCDU() {
 		int top = Integer.parseInt(nTop.getText());
 		int left = Integer.parseInt(nLeft.getText());
 		int height = Integer.parseInt(nHeight.getText());
 		int width = Integer.parseInt(nWidth.getText());
-		CDUWindow.setBounds(left, top, width, height);
+
+		Rectangle rect= new Rectangle();
+		rect.setBounds(left, top, width, height);
+		CDUWindow.redraw(rect);
 	}
 }
