b92effc40418244dce723352d38e7b6c614af3a4
Frits <frits@thuisnet.info>
2015-10-16 16:46:05 +0200

    unit tests & CDUSettings changes

--- src/UnitTests/CDUSettingsTest.java
+++ src/UnitTests/CDUSettingsTest.java
@@ -14,8 +14,16 @@ public class CDUSettingsTest {
 	public void testGetIniFileName() {
 		CDUSettings ini = new CDUSettings();
 		String result= ini.getIniFileName();
-		// should return default iniName "src\\FSCDU.ini"
-		assertEquals("iniFileName NOIT equal", "src\\FSCDU.ini", result);
+		// should return default iniName "src/FSCDU.ini"
+		assertEquals("iniFileName NOT src/FSCDU.ini", "src/FSCDU.ini", result);
+	}
+	
+	@Test
+	public void testGetIniFileName2() {
+		CDUSettings ini = new CDUSettings("abc.def");
+		String result= ini.getIniFileName();
+		// should return default iniName "src/FSCDU.ini"
+		assertEquals("iniFileName NOT 'abc.def'", "abc.def", result);
 	}
 	
 	@Test
@@ -24,7 +32,7 @@ public class CDUSettingsTest {
 		String name1= "Filename1.ini";
 		ini.setIniFileName(name1);
 		String result= ini.getIniFileName();
-		assertEquals("iniFileName NOIT equal", name1, result);
+		assertEquals("iniFileName NOT equal", name1, result);
 	}
 	
 	@Test
--- src/ufmc/CDUSettings.java
+++ src/ufmc/CDUSettings.java
@@ -1,30 +1,79 @@
 package ufmc;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * @author frits.senders
+ *
+ */
 public class CDUSettings {
 
-	public Integer CDUTop;
-	public Integer CDULeft;
-	public Integer CDUHeight;
-	public Integer CDUWidth;
-	public String IOCPServerIP;
-	public Integer IOCPServerPort;
-
+	public Integer CDUTop=10;
+	public Integer CDULeft=10;
+	public Integer CDUHeight=400;
+	public Integer CDUWidth=600;
+	public String  IOCPServerIP="127.0.0.1";
+	public Integer IOCPServerPort=8000;
 	private Properties properties;
-	private String iniFileName = "src\\FSCDU.ini"; //default iniFileName
+	private String iniFileName = "";
+	
+	String defaultFileName = "src/FSCDU.ini"; //default iniFileName
 
+	/* 
+	 * default constructor 
+	 */
 	public CDUSettings() {
+		init(defaultFileName);
+	}
+	
+	/* 
+	 * constructor specify ini-file name
+	 */
+	public CDUSettings( String fileName) {
+		init(fileName);
+	}
+
+	/* 
+	 * initialization 
+	 */
+	private void init( String fileName) {
 		properties = new Properties();
+		setIniFileName(fileName);
+	}
+	
+	/* 
+	 * return from which ini-file we are reading/writing
+	 */
+	public String getIniFileName() {
+		return iniFileName;
+	}
+
+	/* 
+	 * set new ini-file, create it if necessary
+	 * AND read it!
+	 */
+	public void setIniFileName(String iniFileName) {
+		this.iniFileName = iniFileName;
 		try {
-			readInifile();
+			readInifile();	
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
 
+	/* 
+	 * read the ini-file, create it if necessary
+	 * AND read it!
+	 */
 	public void readInifile() {
+		// if file does not extst, create it
+		if (!iniFileExists(iniFileName)){
+			createFile(iniFileName);
+			writeInifile();
+		}
 		try {
 			properties.load(new FileInputStream(iniFileName));
 			CDUTop = Integer.parseInt(properties.getProperty("CDUTop"));
@@ -38,14 +87,9 @@ public class CDUSettings {
 		}
 	}
 
-	public String getIniFileName() {
-		return iniFileName;
-	}
-
-	public void setIniFileName(String iniFileName) {
-		this.iniFileName = iniFileName;
-	}
-
+	/* 
+	 * write proprerties to the ini-file 
+	 */
 	public void writeInifile() {
 		properties.put("CDUTop", CDUTop.toString());
 		properties.put("CDULeft", CDULeft.toString());
@@ -57,6 +101,20 @@ public class CDUSettings {
 			properties.store(new FileOutputStream(iniFileName), "/* properties updated */");
 		} catch (Exception e) {
 			System.out.println(e);
+		}
+	}
+	
+	private boolean iniFileExists(String fileName){
+		File f = new File(fileName); 
+		return f.isFile();
+	}
+
+	private void createFile(String fileName){
+		File f = new File(fileName); 
+		try {
+			f.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
